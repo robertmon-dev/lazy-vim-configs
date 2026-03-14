b060b5743e9cc5dfb1ab7a415cb39cf6bc858519
Robert Moń <m0ni3v@gmail.com>
2026-03-14 11:38:32 +0100

    feat: add suda for root access file modification

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,5 +19,27 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   callback = function(args)
     vim.lsp.buf.format({ bufnr = args.buf, timeout_ms = 1000 })
   end,
+  desc = "Trim trailing whitespace on save",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = my_autocmds,
+  callback = function(args)
+    vim.lsp.buf.format({ bufnr = args.buf, timeout_ms = 1000 })
+  end,
   desc = "Format with LSP after save",
+})
+
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.readonly then
+      vim.defer_fn(function()
+        vim.api.nvim_echo({
+          { " ⚠️  WARNING: As this file is READONLY! \n", "WarningMsg" },
+          { " You can save changes only after password provided.\n", "Normal" },
+        }, true, {})
+      end, 500)
+    end
+  end,
 })
