593689091a76e159b5473b0b5c785c97be626e7b
Robert Moń <m0ni3v@gmail.com>
2026-03-14 11:49:12 +0100

    fix: drop some documented confs from silicon

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -5,22 +5,18 @@ return {
   lazy = true,
   cmd = "Silicon",
   opts = {
-    font = "JetBrains Mono=18",
 
-    theme = "auto",
+    theme = "Dracula",
     background = p.bg_night,
 
     no_window_controls = true,
     pad_horiz = 56,
     pad_vert = 56,
 
-    drop_shadow = true,
     shadow_blur_radius = 68,
     shadow_offset_y = 20,
     shadow_color = "#000000",
 
     to_clipboard = true,
-
-    watermark_text = "",
   },
 }
