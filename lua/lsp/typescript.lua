f25b3c5e74cbf7be1278d02107cad532b2287568
Robert Moń <m0ni3v@gmail.com>
2026-08-29 11:07:23 +0200

    refactor(lsp): modularize TypeScript LSP settings
    Separated TypeScript and JavaScript settings into distinct variables for better organization. Introduced `monorepo_root_dir` for determining root in monorepo projects, enhancing the flexibility of project configurations.

--- lua/lsp/typescript.lua
+++ lua/lsp/typescript.lua
@@ -1,64 +1,91 @@
+local root_markers = {
+  "pnpm-workspace.yaml",
+  "turbo.json",
+  "nx.json",
+  "lerna.json",
+  ".git",
+}
+
+local function monorepo_root_dir(bufnr, on_dir)
+  local fname = vim.api.nvim_buf_get_name(bufnr)
+  on_dir(vim.fs.root(fname, root_markers))
+end
+
+local watch_options = {
+  watchFile = "useFsEventsOnParentDirectory",
+  watchDirectory = "useFsEvents",
+  fallbackPolling = "dynamicPriority",
+  synchronousWatchDirectory = false,
+  excludeDirectories = {
+    "**/node_modules",
+    "**/.git",
+    "**/dist",
+    "**/build",
+    "**/.next",
+    "**/coverage",
+  },
+}
+
+local inlay_hints_off = {
+  enumMemberValues = { enabled = false },
+  functionLikeReturnTypes = { enabled = false },
+  parameterNames = { enabled = "none" },
+  parameterTypes = { enabled = false },
+  propertyDeclarationTypes = { enabled = false },
+  variableTypes = { enabled = false },
+}
+
+local vtsls_settings = {
+  enableMoveToFileCodeAction = true,
+  autoUseWorkspaceTsdk = true,
+  experimental = {
+    completion = {
+      enableServerSideFuzzyMatch = true,
+      entriesLimit = 50,
+    },
+  },
+  tsserver = {
+    globalPlugins = {},
+  },
+}
+
+local typescript_settings = {
+  preferences = {
+    includePackageJsonAutoImports = "auto",
+    importModuleSpecifier = "non-relative",
+    preferTypeOnlyAutoImports = true,
+  },
+  disableAutomaticTypeAcquisition = true,
+  updateImportsOnFileMove = { enabled = "always" },
+  suggest = {
+    completeFunctionCalls = true,
+    autoImports = true,
+  },
+  validate = { enable = true },
+  tsserver = {
+    maxTsServerMemory = 8192,
+    watchOptions = watch_options,
+  },
+  inlayHints = inlay_hints_off,
+}
+
+local javascript_settings = {
+  updateImportsOnFileMove = { enabled = "always" },
+  suggest = { completeFunctionCalls = true },
+  inlayHints = inlay_hints_off,
+}
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
       vtsls = {
+        root_dir = monorepo_root_dir,
         settings = {
           complete_function_calls = true,
-          vtsls = {
-            enableMoveToFileCodeAction = true,
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-                entriesLimit = 50,
-              },
-            },
-            tsserver = {
-              maxTsServerMemory = 8192,
-              globalPlugins = {},
-            },
-          },
-          typescript = {
-            updateImportsOnFileMove = { enabled = "always" },
-            suggest = {
-              completeFunctionCalls = true,
-              autoImports = true,
-            },
-            validate = { enable = true },
-            tsserver = {
-              watchOptions = {
-                watchFile = "useFsEvents",
-                watchDirectory = "useFsEvents",
-                fallbackPolling = "dynamicPriority",
-                synchronousWatchDirectory = false,
-                excludeDirectories = {
-                  "**/node_modules",
-                  "**/.git",
-                  "**/dist",
-                  "**/build",
-                  "**/.next",
-                  "**/coverage",
-                },
-              },
-            },
-            inlayHints = {
-              enumMemberValues = { enabled = false },
-              functionLikeReturnTypes = { enabled = false },
-              parameterNames = { enabled = "none" },
-              parameterTypes = { enabled = false },
-              propertyDeclarationTypes = { enabled = false },
-              variableTypes = { enabled = false },
-            },
-          },
-          javascript = {
-            updateImportsOnFileMove = { enabled = "always" },
-            suggest = { completeFunctionCalls = true },
-            inlayHints = {
-              parameterNames = { enabled = "none" },
-              variableTypes = { enabled = false },
-            },
-          },
+          vtsls = vtsls_settings,
+          typescript = typescript_settings,
+          javascript = javascript_settings,
         },
       },
     },
